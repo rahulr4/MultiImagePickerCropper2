9b6947ba8e77b339b35aa60fbf4e22ab5d417d40
Rahul <rahul.r4@gmail.com>
2016-05-08 12:32:08 +0530

    deleted all data from master repo

--- multipleImagePickerLib/src/main/java/com/luminous/pick/Action.java
+++
@@ -1,7 +0,0 @@
-package com.luminous.pick;
-
-public class Action {
-
-	public static final String ACTION_PICK = "luminous.ACTION_PICK";
-	public static final String ACTION_MULTIPLE_PICK = "luminous.ACTION_MULTIPLE_PICK";
-}
